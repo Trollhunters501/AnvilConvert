331f914d5093ec4f04b501ebfed7fe97b727e597
Trollhunters501PC <cpcreador@gmail.com>
2024-11-20 13:09:51 -0600

    Actualizar a JSENK2

--- AnvilConvert.js
+++ AnvilConvert.js
@@ -1,21 +1,14 @@
-var AnvilconvertJar;
-var PrefixAnvilcon = "[AnvilConvert] ";
-script.registerScript({
-    name: "AnvilConvert",
-    version: "1.0",
-    description: "Convert MCregion worlds to Anvil!",
-    website: "https://github.com/Trollhunters501/AnvilConvert",
-    authors: ["Creadores Program"]
-});
-script.addEventListener("Enable", function(){
+let AnvilconvertJar;
+let PrefixAnvilcon = "[AnvilConvert] ";
+function enable(){
     console.info(PrefixAnvilcon+"§eLoading...");
     AnvilconvertJar = new NnClassLoader({ urls: ["https://github.com/Awzaw/AnvilConverter/releases/download/v1.0/AnvilConverter.jar"] });
-    manager.createCommand("regionconvert", "Convert MCregion worlds to Anvil!", "AnvilCmd", "Use /regionconvert <world>", [], "anvil.convert.world");
-    console.info("§aDone!");
-});
-script.addEventListener("Disable", function(){
+    manager.createCommand("regionconvert", "Convert MCregion worlds to Anvil!", AnvilCmd, "Use /regionconvert <world>", [], "anvil.convert.world");
+    console.info(PrefixAnvilcon+"§aDone!");
+}
+function disable(){
     console.info(PrefixAnvilcon+"§cBye!");
-});
+};
 function AnvilCmd(sender, args, label){
     if(!sender.hasPermission("anvil.convert.world")){
         sender.sendMessage("§c"+server.getLanguage().translateString("commands.generic.permission"));
@@ -26,10 +19,14 @@ function AnvilCmd(sender, args, label){
         return;
     }
     try{
-    let main = AnvilconvertJar.type("net.minecraft.world.level.storage.AnvilConverter");
-    main.main([server.dataPath+"worlds", args[0]]);
+      let main = AnvilconvertJar.type("net.minecraft.world.level.storage.AnvilConverter");
+      main.main([server.dataPath+"worlds", args[0]]);
     }catch(error){
         sender.sendMessage("§c"+server.getLanguage().translateString("commands.generic.exception"));
         console.info("§cError in command regionconvert", error);
     }
+}
+module.exports = {
+    onEnable: enable,
+    onDisable: disable
 }
